5385342032721fde0023c5645c5dc62f42d5ed31
Nazmul Hoque <coder.nazmul@gmail.com>
2020-04-08 13:17:37 +0600

    not found component designed completed

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>404 Error</h2>
-        </div>
+        <div id="notfound">
+		<div className="notfound">
+			<div className="notfound-404">
+				<h1>Oops!</h1>
+			</div>
+			<h2>404 - Page not found</h2>
+			<p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
+			<a href="/">Go To Homepage</a>
+		</div>
+	</div>
     );
 };
 
