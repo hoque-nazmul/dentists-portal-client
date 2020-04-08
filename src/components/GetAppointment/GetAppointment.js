e62a95f75cfa6d14ca4d519b60144c1736da9a80
Nazmul Hoque <coder.nazmul@gmail.com>
2020-04-08 12:39:22 +0600

    installed & implemented react router

---
+++ src/components/GetAppointment/GetAppointment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GetAppointment = () => {
+    return (
+        <div>
+            <h2>I am from GetAppointment</h2>
+        </div>
+    );
+};
+
+export default GetAppointment;
